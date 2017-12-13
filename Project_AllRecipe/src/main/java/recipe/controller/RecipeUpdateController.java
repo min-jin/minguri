73b203eb582160d6af819064464a65e3435bdd24
gbyman <dongchan-2@hanmail.net>
2017-12-13 16:02:07 +0900

    프로젝트 포크 후 첫 커밋

---
+++ Project_AllRecipe/src/main/java/recipe/controller/RecipeUpdateController.java
@@ -0,0 +1,120 @@
+package recipe.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import recipe.model.Recipe;
+import recipe.model.RecipeDao;
+
+@Controller
+public class RecipeUpdateController {
+	
+	private static final String command = "/update.rec";
+	private static final String gotoPage = "redirect:/list.rec";
+	private static final String getPage = "RecipeUpdateForm";
+	
+	@Autowired
+	private ServletContext servletContext;
+	
+	@Autowired
+	private RecipeDao recipeDao;
+	
+	//RecipeList.jsp에서 수정하기 누르면  GET방식으로 넘어온다
+	@RequestMapping(value = command, method = RequestMethod.GET)
+	public String doActionGet(HttpSession session, @RequestParam(value = "recnum", required = true) int recnum, Model model) {
+		
+		//로그인 안한 사람이 수정하기 누르면 로그인 화면으로 이동
+		if(session.getAttribute("loginfo") == null) {
+			
+			session.setAttribute("destination", "redirect:/update.rec?recnum="+recnum);
+			return "redirect:login.mem";
+		
+		} else { //로그인 되어 있다면 RecipeUpdateForm.jsp로 이동
+			
+			Recipe recipe = recipeDao.GetData(recnum);
+			model.addAttribute("recipe", recipe);
+			return getPage; 
+		}
+		
+	}
+		//RecipeUpdateForm.jsp에서 post방식으로 넘어옴
+		@RequestMapping(value = command, method = RequestMethod.POST)
+		public ModelAndView doActionPost(Recipe recipe) {
+			
+			System.out.println("RecipeUpdateController Post방식");
+			
+			//DEV : 이미지 파일 수정 및 수정 전 파일 지우기
+			// 기존의 사진을 MultipartFile 형식의 multi라는 곳에 담았다 
+			ArrayList<MultipartFile> multi = new ArrayList<MultipartFile>();
+			multi.add(recipe.getMainimage());
+			multi.add(recipe.getImage1());
+			multi.add(recipe.getImage2());
+			multi.add(recipe.getImage3());
+			multi.add(recipe.getImage4());
+		
+			ModelAndView mav = new ModelAndView();
+			
+			String uploadDir = servletContext.getRealPath("resources");
+			System.out.println("RecipeUpdateController uploadDir : "+ uploadDir);
+			//
+			
+			Integer cnt = -1;
+			cnt = recipeDao.UpdateData(recipe);
+			
+			
+			//수정 성공
+			if(cnt != -1) {
+				//Note : 5장 올리고 수정을 5장 미만으로 했을 때 결과 확인 해야 함
+				//DEV :  기존 이미지 파일 5장 일일이 지우는 법에서 한번에 지울 수 있게 해야 함
+				for(int i = 0 ; i < multi.size() ; i++) {
+					
+					File destination = new File(uploadDir + File.separator + multi.get(i).getOriginalFilename());
+					
+					File recipeMainimage = new File(uploadDir + File.separator + recipe.getMainimage());//기존 이미지 파일
+					File image1 = new File(uploadDir + File.separator + recipe.getImage1()); 
+					File image2 = new File(uploadDir + File.separator + recipe.getImage2());
+					File image3 = new File(uploadDir + File.separator + recipe.getImage3());
+					File image4 = new File(uploadDir + File.separator + recipe.getImage4());
+					
+					recipeMainimage.delete();
+					image1.delete();
+					image2.delete();
+					image3.delete();
+					image4.delete();
+					
+					try {
+						multi.get(i).transferTo(destination);
+					} catch(IllegalStateException e) {
+						e.printStackTrace();
+					} catch(IOException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				mav.setViewName(gotoPage);// 기존 이미지 삭제하고 redirect:/list.rec 로 이동 
+				
+				
+			} else { //수정 실패 후 RecipeUpdateForm.jsp로 다시 이동
+				
+				mav.setViewName(getPage);// RecipeUpdateForm.jsp로 이동
+				
+			}//if~else
+			
+			return mav;
+		
+	}
+
+}
---
+++ Project_AllRecipe/src/main/webapp/WEB-INF/recipe/RecipeDetailView.jsp
@@ -0,0 +1,98 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ include file="../MainTop.jsp" %>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+레시피 상세 보기
+<c:set var="mainImage" value="http://localhost:9090${pageContext.request.contextPath }/resources/${recipe.mainimage }"/>
+<c:set var="image1" value="http://localhost:9090${pageContext.request.contextPath }/resources/${recipe.image1 }"/>
+<c:set var="image2" value="http://localhost:9090${pageContext.request.contextPath }/resources/${recipe.image2 }"/>
+<c:set var="image3" value="http://localhost:9090${pageContext.request.contextPath }/resources/${recipe.image3 }"/>
+<c:set var="image4" value="http://localhost:9090${pageContext.request.contextPath }/resources/${recipe.image4 }"/>
+
+
+<div align="center">
+	<table border="1">
+		<tr>
+			<td>
+				<img src="${mainImage }"/>
+			</td>
+			
+			<td align="center">
+				<table border="1">
+					<tr height="50">
+						<td width="120">요리명</td>
+						<td width="300"><c:out value="${recipe.psubject }"/></td>
+					</tr>
+					
+					<tr height="50">
+						<td width="120">간단한 설명</td>
+						<td width="300"><c:out value="${recipe.shortintro }"/></td>
+					</tr>
+				
+					<tr height="50">
+						<td width="120">재료</td>
+						<td width="300"><c:out value="${recipe.ingredient }"/></td>
+					</tr>
+					
+					
+					
+					<tr height="50">
+						<td width="120">동영상 </td>
+						<td width="300"><c:out value="${recipe.movieurl }"/></td>
+					</tr>
+					
+					<tr height="50">
+						<td width="120">재료</td>
+						<td width="300"><c:out value="${recipe.ingredient }"/></td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		
+			<tr>
+				<td>
+					<img src="${image1 }"/>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<img src="${image2 }"/>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<img src="${image3 }"/>
+				</td>
+			</tr>
+			
+			<tr>
+				<td>
+					<img src="${image4 }"/>
+				</td>
+			</tr>
+			
+			<tr>
+				<td width="120">설명</td>
+				<td width="300"><c:out value="${recipe.intro }"/></td>
+			</tr>
+		
+	</table>
+</div>
+<input type="button" value="레시피" onclick="goList();"/>
+</body>
+
+<script type="text/javascript">
+	function goList(){// 버튼 누르면 레시피로 이동
+		location.href='list.rec'; //get 방식
+	}
+</script>
+<%@include file="../MainBottom.jsp" %>
+</html>
